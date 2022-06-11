dc0fecbeab9776e8f8ada2c5e71df1e0158c3a95
Ganesh <ganeshkumar1092@gmail.com>
2022-06-11 23:46:15 +0530

    basic code added

---
+++ src/app/line-chart/line-chart.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-line-chart',
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.scss']
+})
+export class LineChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createChartLine()
+  }
+
+  private createChartLine(): void {
+    let date = new Date();
+    const data: any[] = [];
+
+    for (let i = 0; i < 10; i++) {
+      date.setDate(new Date().getDate() + i);
+      data.push([`${date.getDate()}/${date.getMonth() + 1}`, this.getRandomNumber(0, 1000)]);
+    }
+
+    const chart = Highcharts.chart('chart-line', {
+      chart: {
+        type: 'line',
+      },
+      title: {
+        text: 'Line Chart',
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: false,
+      },
+      yAxis: {
+        title: {
+          text: null,
+        }
+      },
+      xAxis: {
+        type: 'category',
+      },
+      tooltip: {
+        headerFormat: `<div>Date: {point.key}</div>`,
+        pointFormat: `<div>{series.name}: {point.y}</div>`,
+        shared: true,
+        useHTML: true,
+      },
+      series: [{
+        name: 'Amount',
+        data,
+      }],
+    } as any);
+
+    setInterval(() => {
+      date.setDate(date.getDate() + 1);
+      chart.series[0].addPoint([`${date.getDate()}/${date.getMonth() + 1}`, this.getRandomNumber(0, 1000)], true, true);
+    }, 1500);
+  }
+  
+  private getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+}
